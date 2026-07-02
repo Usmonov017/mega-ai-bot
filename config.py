753ae6586f289e0625b2962384545a80d629123c
Usmonov017 <usmonjonov571@gmail.com>
2026-07-02 16:18:22 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Telegram Bot Token
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7685075588:AAEcDAVyk7ZjEOk8ojdJeshDXcaORJFAGCg")
+
+# Gemini AI API kaliti (Kelgusida ishlatamiz)
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
+
+# Administratorlar ro'yxati (Bu yerga o'zingizning Telegram ID raqamingizni yozishingiz mumkin)
+ADMINS = [123456789]  # 123456789 o'rniga o'z ID-raqamingizni qo'ysangiz ham bo'ladi
