7733f45967d7e0f20a681e379707354b9e420658
Usmonov017 <usmonjonov571@gmail.com>
2026-07-03 23:50:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ logging.basicConfig(level=logging.INFO)
 
 # Muhit o'zgaruvchilari (Config)
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-PUBLIC_CHANNEL = os.getenv("PUBLIC_CHANNEL", "@MoviTimeUz")
-SERVER_CHANNEL = os.getenv("SERVER_CHANNEL", "@MoviTimeUz")
+PUBLIC_CHANNEL = os.getenv("PUBLIC_CHANNEL")
+SERVER_CHANNEL = os.getenv("SERVER_CHANNEL")
 GEMINI_KEY = os.getenv("GEMINI_API_KEY")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "123456789"))
 
